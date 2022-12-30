b63f58ba08344d8602f5672fc75310b034eafc29
git-leon <xleonhunter@gmail.com>
2022-12-29 19:21:11 -0500

    updated method stubs

--- js/main/NumberList.js
+++ js/main/NumberList.js
@@ -8,31 +8,19 @@ class NumberList {
     // Method to find the sum of all numbers in the number list
     // @return {number} The sum of all numbers in the number list
     sum() {
-      let sum = 0;
-      let i = 0;
-      while (i < this.numbers.length) {
-        sum += this.numbers[i];
-        i++;
-      }
-      return sum;
+      return null;
     }
   
     // Method to find the product of all numbers in the number list
     // @return {number} The product of all numbers in the number list
     product() {
-      let product = 1;
-      let i = 0;
-      while (i < this.numbers.length) {
-        product *= this.numbers[i];
-        i++;
-      }
-      return product;
+      return null;
     }
   
     // Method to find the average of all numbers in the number list
     // @return {number} The average of all numbers in the number list
     average() {
-      return this.sum() / this.numbers.length;
+      return null;
     }
   }
   
